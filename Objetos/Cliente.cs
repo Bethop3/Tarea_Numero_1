af3956a5485da49e38aeddc37e05a0ec9ce562f6
Yahir <yahir.chi.chi@gmail.com>
2023-05-22 13:39:22 -0500

    Cambios1

--- Objetos/Cliente.cs
+++ Objetos/Cliente.cs
@@ -8,5 +8,44 @@ namespace Tarea1.Objetos
 {
     internal class Cliente
     {
+        public string Nombre { get; set; }
+        public long Telefono { get; set;}
+        public string Correo { get; set; } 
+        
+        public static void Imprimir(Cliente cliente)
+        {
+            Console.Clear();
+            Console.WriteLine("Nombre del cliente: ");
+            Console.WriteLine(cliente.Nombre);
+            Console.WriteLine("Telefono del cliente:");
+            Console.WriteLine(cliente.Telefono);
+            Console.WriteLine("Correo del cliente: ");
+            Console.WriteLine(cliente.Correo);
+            Console.WriteLine("\n");
+        }
+        public static Cliente Registro()
+        {
+            Cliente cliente = new Cliente();
+            bool a = true;
+            while (a)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese su Nombre:");
+                    cliente.Nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese su telefono:");
+                    cliente.Telefono = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese su correo:");
+                    cliente.Correo = Console.ReadLine();
+                    a = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        
+            return cliente;
+        }
     }
 }
--- Objetos/Productos.cs
+++ Objetos/Productos.cs
@@ -8,5 +8,63 @@ namespace Tarea1.Objetos
 {
     internal class Productos
     {
+        public int Descuento(int Precio)
+        {
+            if (Precio >= 5000) 
+            {
+                int des = (Precio / 100) * 10;
+                Console.WriteLine("\n Solo Aplica el descuento: \n");
+                return des;
+            }
+            
+            return 0;
+        }
+        public string Intereses(int Precio)
+        {
+            return Precio >= 8000 ? "Aplica" : "No aplica";
+        }
+        public string Interes (int Precio)
+        {
+            return Precio >= 10000 ? "Aplica" : "No Aplica";
+        }
+            
+        public void Ingresar()
+        {
+            Console.WriteLine("Instrucciones: \n");
+            Console.WriteLine("Si es mayor o igual a 5000 se le aplicara un descuento del 10%");
+            Console.WriteLine("Si es mayor o igual a 8000, se aplicara una opcion de pago a 3 meses sin intereses (Tendra que elegir si quiere el descuento del 10% o pagar a meses sin interese)");
+            Console.WriteLine("Si es mayor a 10000, se le aplicara una opcion de pago a 6 o 10 meses sin intereses (Tendra que elegir si quiere el descuento del 10% o pagar a meses sin interese)");
+            Console.WriteLine("Ingrese el Precio del articulo a comprar: ");
+            try
+            {
+                int Articulo = int.Parse(Console.ReadLine());
+                double descuento = this.Descuento(Articulo);
+                string Condicion = this.Intereses(Articulo);
+                string Condicion1 = this.Interes(Articulo);
+                switch (Condicion)
+                {
+                    case "Aplica":
+                        switch (Condicion1)
+                        {
+                            case "Aplica":
+
+                                break;
+
+                            case "No Aplica":
+
+                                break;
+                        }
+                        break;
+
+                    case "No aplica": 
+                        Console.WriteLine("El precio es de "+(Articulo+descuento));
+                        break;
+                }
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
